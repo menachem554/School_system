598f5839210eb11e713809b2470659288537b09e
menachem554 <mendyrivkin@gmail.com>
2021-09-02 08:05:52 +0300

    m

--- src/routes/student.router.ts
+++ src/routes/student.router.ts
@@ -3,7 +3,7 @@ import * as studentController from '../controllers/students.controller';
 
 const router = express.Router();
 
-router.post('/newstudent', studentController.postStudent);
+router.post('/newStudent', studentController.postStudent);
 router.get('/SID', studentController.getStudent);
 router.put('/editStudent', studentController.editStudent);
 router.delete('/del', studentController.delStudent);
--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,16 @@ import studentrouter from './routes/student.router';
 require('dotenv').config();
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.DATABASE_URL as string, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', (error: Error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+mongoose
+  .connect(process.env.DATABASE_URL as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/api/tachers', tacherRouter);
